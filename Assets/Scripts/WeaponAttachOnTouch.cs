524112393a8876e2b51ca1da3e7d567f7e8c5b97
Ayline Vargas <ayline123@gmail.com>
2025-10-23 00:53:24 -0300

    otro cambio

--- Assets/Scripts/WeaponAttachOnTouch.cs
+++ Assets/Scripts/WeaponAttachOnTouch.cs
@@ -3,29 +3,42 @@ using UnityEngine;
 public class WeaponAttachOnTouch : MonoBehaviour
 {
     public Transform weaponHolder;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Weapon"))
         {
-            // Desactiva físicas ANTES de mover el arma
+            // Desactiva físicas del arma
             Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb)
             {
-                rb.isKinematic = true;   // evita que afecte al jugador
+                rb.isKinematic = true;
                 rb.useGravity = false;
             }
 
             Collider col = other.GetComponent<Collider>();
             if (col)
             {
-                col.enabled = false;     // evita colisiones futuras
+                col.enabled = false;
             }
 
-            // Ahora sí, pega el arma a la mano
+            // Pega el arma a la mano
             other.transform.SetParent(weaponHolder);
             other.transform.localPosition = Vector3.zero;
             other.transform.localRotation = Quaternion.identity;
+
+            // Activa animaciones de "con arma"
+            anim.SetBool("HasWeapon", true);
         }
+    }
+    void DropWeapon()
+    {
+        anim.SetBool("HasWeapon", false);
     }
 }
